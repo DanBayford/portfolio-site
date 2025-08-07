68ef1972bf400e9b75c3765d4b16f11113f8acc1
Dan Bayford <danbeeweb@gmail.com>
2025-08-07 10:02:58 +0100

    post deploy config

--- web/core/settings.py
+++ web/core/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [str(BASE_DIR / "core/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 WAGTAIL_SITE_NAME = "Dan's Portfolo"
 WAGTAILADMIN_BASE_URL = "https://bayford.dev"
