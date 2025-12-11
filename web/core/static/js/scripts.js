eb55db76b5af94d6e3b422f9c430d03551993d88
Dan Bayford <danbeeweb@gmail.com>
2025-12-11 13:42:58 +0000

    website snags

--- web/core/static/js/scripts.js
+++ web/core/static/js/scripts.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Init syntax highlighter
   if (window.hljs) {
     window.hljs.highlightAll();
   } else {
     console.error("Highlight.js not loaded.");
   }
+  // Add extra attributes to <a> tags in streamfields (ie external links)
+  const streamFieldExternalLinks = document.querySelectorAll(".stream_field a");
+  streamFieldExternalLinks.forEach((link) => {
+    const isExternal =
+      link.hostname && link.hostname !== window.location.hostname; // Check if external host
+    if (isExternal) {
+      link.target = "_blank";
+      link.rel = "noopener norefferer";
+    }
+  });
 });
